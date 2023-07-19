fe3762da6f453826020f5027836f710ec3c645c0
dalandroid <tkdgns0310@gmail.com>
2023-07-19 13:39:15 +0900

    branch text

---
+++ app/src/main/java/kr/care_pet/service/app/navi/activity/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package kr.care_pet.service.app.navi.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.care_pet.service.app.navi.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
